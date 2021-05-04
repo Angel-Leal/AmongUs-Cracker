a9e50042bf499b5354f6fa011f400087b65a3ebe
Angel Leal <dashtakegamer@gmail.com>
2021-05-04 09:30:53 -0500

    First Version

---
+++ Among Us Cracker/Inferface.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Among_Us_Cracker
+{
+    partial class Inferface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inferface));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCrack = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto Slab", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AMONG US CRACKER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Creado por FoxPlaysAlone";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(106, 146);
+            this.txtNombre.MaxLength = 10;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(157, 27);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre:";
+            // 
+            // btnCrack
+            // 
+            this.btnCrack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCrack.BackgroundImage")));
+            this.btnCrack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCrack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCrack.FlatAppearance.BorderSize = 0;
+            this.btnCrack.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCrack.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCrack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrack.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrack.Location = new System.Drawing.Point(67, 192);
+            this.btnCrack.Name = "btnCrack";
+            this.btnCrack.Size = new System.Drawing.Size(150, 32);
+            this.btnCrack.TabIndex = 4;
+            this.btnCrack.Text = "Crackear";
+            this.btnCrack.UseVisualStyleBackColor = true;
+            this.btnCrack.Click += new System.EventHandler(this.btnCrack_Click);
+            this.btnCrack.MouseEnter += new System.EventHandler(this.btnCrack_MouseEnter);
+            this.btnCrack.MouseLeave += new System.EventHandler(this.btnCrack_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Among_Us_Cracker.Properties.Resources.FoxPlaysAlone;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Inferface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(216)))), ((int)(((byte)(53)))));
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCrack);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Inferface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Among Us Cracker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCrack;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Among Us Cracker/Inferface.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Among_Us_Cracker
+{
+    public partial class Inferface : Form
+    {
+
+        string source = "playerPrefs";
+        string destination = Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.ApplicationData), "../LocalLow/Innersloth/Among Us/playerPrefs");
+        string crack = "a,1,0,1,False,False,False,0,False,False,0,17,29,1,0,0,0,False,1,True,0,1,,1,1,2000,04/01/2021 09:39:27,1,False,CrackeadoPorFoxPlaysalone@gmail.com,True";
+
+        public Inferface()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://youtube.com/c/FoxPlaysAlone");
+        }
+
+        string replaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+
+        private void copyFile()
+        {
+            if (File.Exists(destination))
+            {
+                try
+                {
+                    File.Delete(destination);
+                }
+                catch
+                {
+                    MessageBox.Show("No se pudo reemplazar el archivo.");
+                }
+            }
+            File.Copy(source, destination);
+        }
+
+        private void createFile()
+        {
+            using (StreamWriter sw = File.CreateText(source))
+            {
+                sw.WriteLine(crack);
+            }
+        }
+
+        private void btnCrack_Click(object sender, EventArgs e)
+        {
+            btnCrack.Image = Properties.Resources.btn;
+
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Por favor ingresa un nombre", "Error!");
+            }
+            else
+            {
+                if (!File.Exists(source))
+                {
+                    createFile();
+                }
+                else
+                {
+                    File.Delete(source);
+                    createFile();
+                }
+                string text = File.ReadAllText(source);
+                string name = text.Substring(0, text.IndexOf(','));
+                text = replaceFirst(text, name, txtNombre.Text);
+                File.WriteAllText(source, text);
+            copyFile();
+            File.Delete(source);
+            MessageBox.Show("El juego se ha crackeado con éxito.\n\nSuscribete al canal!", "Juego crackeado.");
+            }
+
+        }
+
+        private void btnCrack_MouseEnter(object sender, EventArgs e)
+        {
+            btnCrack.Image = Properties.Resources.btn_hover;
+        }
+
+        private void btnCrack_MouseLeave(object sender, EventArgs e)
+        {
+            btnCrack.Image = Properties.Resources.btn;
+        }
+
+    }
+}
